8dfe8c836c59b146fb86b2ba5d95e32e8ee289b8
chengzhou <chengzhou@wacai.com>
2023-01-12 16:31:55 +0800

    开始梳理保护模式

--- source/os.c
+++ source/os.c
@@ -2,3 +2,15 @@
  * 功能：32位代码，完成多任务的运行
  */
 #include "os.h"
+
+// 类型定义
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
+struct {uint16_t limit_l, base_l, basehl_attr, base_limit;}gdt_table[256] __attribute__((aligned(8))) = {
+    // 0x00cf9a000000ffff - 从0地址开始，P存在，DPL=0，Type=非系统段，32位代码段（非一致代码段），界限4G，
+    [KERNEL_CODE_SEG / 8] = {0xffff, 0x0000, 0x9a00, 0x00cf},
+    // 0x00cf93000000ffff - 从0地址开始，P存在，DPL=0，Type=非系统段，数据段，界限4G，可读写
+    [KERNEL_DATA_SEG/ 8] = {0xffff, 0x0000, 0x9200, 0x00cf},
+};
--- source/os.h
+++ source/os.h
@@ -4,4 +4,8 @@
 #ifndef OS_H
 #define OS_H
 
+// 各选择子
+#define KERNEL_CODE_SEG         (1 * 8)
+#define KERNEL_DATA_SEG         (2 * 8)
+
 #endif // OS_H
